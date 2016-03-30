0170fd3794455163e0c25e8e6f5761555e383742
Christine <christine.geidemark@outlook.com>
2016-03-30 13:03:22 +0200

    DB

--- src/KoLappen/Startup.cs
+++ src/KoLappen/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace KoLappen
 {
@@ -14,9 +16,24 @@ namespace KoLappen
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
-            //lägg till connString här
+        {                       
             services.AddMvc();
+
+            //Connection String
+            var identityConnString = @"Data Source=kolappen.database.windows.net;Initial Catalog=Projekt3;Integrated Security=False;User ID=AWAP3;Password=Woaow2016;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            
+            //services.AddEntityFramework()
+            //    .AddSqlServer()
+            //    .AddDbContext<PaskContext>(o => o.UseSqlServer(identityConnString));
+
+            //lagra användaren i databasen
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDbContext>() //vart det skall lagras
+                .AddDefaultTokenProviders(); //standard inställingar, regler för lösenord
+
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<IdentityDbContext>(o => o.UseSqlServer(identityConnString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +41,14 @@ namespace KoLappen
         {
             app.UseStaticFiles();
             app.UseDeveloperExceptionPage();
+
+            app.UseCookieAuthentication(o =>
+            {
+                o.AutomaticChallenge = true;
+                //o.LoginPath = new PathString("/Members/Login/");
+            });
+
+            app.UseIdentity();
             app.UseMvcWithDefaultRoute();
         }
 
