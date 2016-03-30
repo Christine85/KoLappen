41b145b327bec4a2f7dd70e5b2838fcec233a052
Christine <christine.geidemark@outlook.com>
2016-03-30 14:32:20 +0200

    Klasserna mot databasen

---
+++ src/KoLappen/Models/JobArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoLappen.Models
+{
+    public class JobArea
+    {
+        [Key]
+        public int JobAreaID { get; set; }
+        public string City { get; set; }
+    }
+}
