b019701571291bca7adb5c38d13af4d874329b59
Christine <christine.geidemark@outlook.com>
2016-03-30 14:40:46 +0200

    DB COntext

---
+++ src/KoLappen/Models/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoLappen.Models
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<JobArea> JobAreas { get; set; }
+        public DbSet<Education> Educations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<JobArea>().ToTable("JobAreas");
+            modelBuilder.Entity<Education>().ToTable("Educations");
+        }
+    }
+}
---
+++ src/KoLappen/Models/Education.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoLappen.Models
+{
+    public class Education
+    {
+        [Key]
+        public int EducationID { get; set; }
+        public string CourseName { get; set; }
+        public string Semester { get; set; }
+        public string Location { get; set; }
+    }
+}
