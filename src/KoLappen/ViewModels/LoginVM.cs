e6f5797058e38a1fe778d3927a6dbbdb868beb24
Vivst <amin.v.naeri@gmail.com>
2016-04-01 10:01:57 +0200

    ´Push för Mattias

--- src/KoLappen/ViewModels/LoginVM.cs
+++ src/KoLappen/ViewModels/LoginVM.cs
@@ -17,7 +17,7 @@ namespace KoLappen.ViewModels
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me")]
-        public bool RememberMe { get; set; }
+        //[Display(Name = "Remember me")]
+        //public bool RememberMe { get; set; }
     }
 }
