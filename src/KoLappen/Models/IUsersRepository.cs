a89101fc3cbca8dffb85d3dbb791528252a408fe
Babak Fatahzadeh <babak.fatahzadeh@gmail.com>
2016-04-01 16:13:05 +0200

    Fixat lägga till användare och lite annat smått och gott

--- src/KoLappen/Models/IUsersRepository.cs
+++ src/KoLappen/Models/IUsersRepository.cs
@@ -14,8 +14,10 @@ namespace KoLappen.Models
     public interface IUsersRepository
     {
         void AddUser(AddUserViewModel viewModel, string userId);
-        IEnumerable<SelectListItem> GetEducations();
-        IEnumerable<SelectListItem> GetJobAreas();
+        Education GetEducation(int eduId);
+        List<UserJobArea> GetUserJobAreas(int jobId);
+        //IEnumerable<SelectListItem> GetEducations();
+        //IEnumerable<SelectListItem> GetJobAreas();
     }
 
     public class DbUsersRepository : IUsersRepository
@@ -35,37 +37,92 @@ namespace KoLappen.Models
                 UserName = viewModel.Email,
                 Firstname = viewModel.Firstname,
                 Lastname = viewModel.Lastname,
-                Education = viewModel.EducationID,
+                Education = GetEducation(viewModel.Education),
+                //UserJobAreas = GetUserJobAreas(Convert.ToInt32(viewModel.JobArea)),
+                UserJobAreas = null,
                 NeedHelp = false,
                 HelpTime = DateTime.Now,
                 ProfilePic = null
+            };
+
+            userProfile.Education = new Education
+            {
+                EducationID = viewModel.Education
             };
 
             dbContext.Users.Add(userProfile);
             dbContext.SaveChanges();
         }
 
-        public IEnumerable<SelectListItem> GetEducations()
+        public List<UserJobArea> GetUserJobAreas(int jobId)
         {
-            List<SelectListItem> educationsList = new List<SelectListItem>();
-            var eduList = dbContext.Educations.Select(x =>
-                                new SelectListItem
-                                {
-                                    Value = x.EducationID.ToString(),
-                                    Text = x.CourseName
-                                });
-            return eduList;
+            List<UserJobArea> jobAreas = new List<UserJobArea>();
+            //var jobList = dbContext.JobAreas.Select(j =>
+            //new UserJobArea
+            //{
+            //    JobAreaID = j.JobAreaID,
+
+            //    City = j.City,
+            //    UserJobAreas = j.UserJobAreas
+            //});
+            //foreach (var job in jobList)
+            //{
+            //    jobAreas.Add(job);
+            //}
+            return jobAreas;
         }
-        public IEnumerable<SelectListItem> GetJobAreas()
+
+        public Education GetEducation(int eduId)
         {
-            List<SelectListItem> educationsList = new List<SelectListItem>();
-            var jobList = dbContext.JobAreas.Select(x =>
-                                new SelectListItem
-                                {
-                                    Value = x.JobAreaID.ToString(),
-                                    Text = x.City
-                                });
-            return jobList;
+            Education education = new Education();
+            var eduList = dbContext.Educations.Select(e =>
+            new Education
+            {
+                EducationID = e.EducationID,
+                CourseName = e.CourseName,
+                Semester = e.Semester,
+                Location = e.Location,
+                Users = null
+
+            });
+            education = eduList.Single(e => e.EducationID == eduId);
+            return education;
         }
+        
+
+        //public IEnumerable<User> GetUser(int educationId)
+        //{
+        //    List<User> userList = new List<User>();
+        //    var usList = dbContext.Users.Select(x=>
+        //    new User
+        //    {
+        //        UserID = x.UserID,
+        //        UserName = x.UserName,
+        //        Firstname = x.Firstname,
+        //        Lastname = x.Lastname,
+        //    })
+        //}
+        //public IEnumerable<SelectListItem> GetEducations()
+        //{
+        //    List<SelectListItem> educationsList = new List<SelectListItem>();
+        //    var eduList = dbContext.Educations.Select(x =>
+        //                        new SelectListItem
+        //                        {
+        //                            Value = x.EducationID.ToString(),
+        //                            Text = x.CourseName
+        //                        });
+        //    return eduList;
+        //}
+        //public IEnumerable<SelectListItem> GetJobAreas()
+        //{
+        //    List<SelectListItem> educationsList = new List<SelectListItem>();
+        //    var jobList = dbContext.JobAreas.Select(x =>
+        //                        new SelectListItem
+        //                        {
+        //                            Value = x.JobAreaID.ToString(),
+        //                            Text = x.City
+        //                        });
+        //    return jobList;
+        //}
     }
 }
