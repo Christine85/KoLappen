f3d3a15727ff7872f4531034d4cc66b31221ac46
Vivst <amin.v.naeri@gmail.com>
2016-04-04 13:32:30 +0200

    Enhetstestning

--- src/KoLappen/Controllers/HomeController.cs
+++ src/KoLappen/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace KoLappen.Controllers
             this.signInManager = signInManager;
         }
         // GET: /<controller>/
-        public async Task<IActionResult> Index()
+        public /*async Task*/IActionResult Index()
         {
             //var autoLogin = true;
 
---
+++ src/KolappenTTD/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KolappenTTD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KolappenTTD")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a98c578d-616d-4177-b1b2-bce529d5aed5")]
