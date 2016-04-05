d1a31e81d0155a6b5da88a7636a2581b4196564b
Babak Fatahzadeh <babak.fatahzadeh@gmail.com>
2016-04-05 10:12:42 +0200

    Uppdatering av databasen

---
+++ src/KoLappen/Models/Course.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace KoLappen.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public List<Education> Educations { get; set; }
+    }
+}
