506d1b59adf51c2a1e6c3fa50f72ad5c33baf61d
Babak Fatahzadeh <babak.fatahzadeh@gmail.com>
2016-04-05 14:44:42 +0200

    fixa stavfel

--- src/KoLappen/Models/TeacherEducation.cs
+++ src/KoLappen/Models/TeacherEducation.cs
@@ -2,7 +2,7 @@
 {
     public class TeacherEducation
     {
-        public int TeacherEducactionId { get; set; }
+        public int TeacherEducationId { get; set; }
         public int TeacherId { get; set; }
         public Teacher Teacher { get; set; }
         public int EducationId { get; set; }
