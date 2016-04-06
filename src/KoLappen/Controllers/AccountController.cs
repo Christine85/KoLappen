2dc869c1d4dc97acef0ed0c153d8a67def8994a1
Babak Fatahzadeh <babak.fatahzadeh@gmail.com>
2016-04-06 10:55:10 +0200

    Fixade registrering

--- src/KoLappen/Controllers/AccountController.cs
+++ src/KoLappen/Controllers/AccountController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Identity;
@@ -8,7 +6,6 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Authorization;
 using KoLappen.ViewModels;
 using KoLappen.Models;
-using Microsoft.AspNet.Mvc.Rendering;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,21 +17,21 @@ namespace KoLappen.Controllers
         UserManager<IdentityUser> userManager;
         SignInManager<IdentityUser> signInManager;
         IdentityDbContext contextIdentity;
-        IUsersRepository usersRepository;
+        IAccountRepository accountRepository;
 
         public AccountController(
             DBContext dbContext,
             UserManager<IdentityUser> userManager, //skapa ny användare
             SignInManager<IdentityUser> signInManager, //logga in
             IdentityDbContext contextIdentity,
-            IUsersRepository usersRepository
+            IAccountRepository usersRepository
             )
         {
             this.dbContext = dbContext;
             this.userManager = userManager;
             this.signInManager = signInManager;
             this.contextIdentity = contextIdentity;
-            this.usersRepository = usersRepository;
+            this.accountRepository = usersRepository;
         }
         // GET: /<controller>/
         public IActionResult Index()
@@ -69,19 +66,43 @@ namespace KoLappen.Controllers
             }
             */
 
-            var result = await signInManager.PasswordSignInAsync(viewModel.UserName, viewModel.Password, false, false);
+            var user = dbContext.Users.Single(o => o.UserName == viewModel.UserName);
+            var aspUser = contextIdentity.Users.Single(o => o.UserName == viewModel.UserName);
+             
 
-            //om admin eller lärare loggar in, skall de få en annan view
-            var user = contextIdentity.Users.Single(o => o.UserName == viewModel.UserName);
-            if (await userManager.IsInRoleAsync(user, "Admin"))
+            // Om användarprofilen ('Users' tabellen) är komplett så loggas användaren in
+            if (user.RegistrationComplete == true)
             {
-                return RedirectToAction(nameof(AdminController.Index), "Admin");
+                var result = await signInManager.PasswordSignInAsync(viewModel.UserName, viewModel.Password, false, false);
+
+                //om admin eller lärare loggar in, skall de få en annan view
+                if (await userManager.IsInRoleAsync(aspUser, "Admin"))
+                {
+                    return RedirectToAction(nameof(AdminController.Index), "Admin");
+                }
+                else if (await userManager.IsInRoleAsync(aspUser, "Lärare"))
+                {
+                    return RedirectToAction(nameof(TeacherController.Index), "Teacher");
+                }
+                return RedirectToAction(nameof(HomeController.Index), "home");
             }
-            else if (await userManager.IsInRoleAsync(user, "Lärare"))
+
+            // Annars uppmanas användaren att färdigställa sin profil
+
+            if (aspUser.EmailConfirmed == true && user.RegistrationComplete == false)
             {
-                return RedirectToAction(nameof(TeacherController.Index), "Teacher");
+                return RedirectToAction(nameof(AccountController.CompleteRegistration), "account");
             }
-            return RedirectToAction(nameof(HomeController.Index), "home");
+
+            if (aspUser.EmailConfirmed == false)
+                return RedirectToAction(nameof(AccountController.CompleteRegistration), "account");
+
+
+            else
+                return RedirectToAction(nameof(AccountController.CompleteRegistration), "login");
+
+
+
         }
 
 
@@ -93,20 +114,22 @@ namespace KoLappen.Controllers
 
 
         [AllowAnonymous]
-        public ActionResult CreateUser()
+        public ActionResult RegisterUser()
         {
             //ViewBag.Educations = new SelectList(dbContext.Educations, "EducationID", "CourseName");
             //ViewBag.JobAreas = new SelectList(dbContext.JobAreas, "JobAreaID", "JobAreaID");
             //viewModel.allEducations = usersRepository.GetAllEducations();
             //viewModel.allJobAreas = usersRepository.GetJobAreas();
-            var viewModel = new CreateUserViewModel();
-            return View();
+            var model = accountRepository.GetRegistrationOptions();
+            return View(model);
+            //var viewModel = new RegisterUserViewModel();
+            //return View();
         }
 
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> AddUser(CreateUserViewModel model)
+        public async Task<IActionResult> RegisterUser(RegisterUserViewModel model)
         {
             if (!ModelState.IsValid)
                 return View(model);
@@ -124,8 +147,8 @@ namespace KoLappen.Controllers
 
             if (result.Succeeded)
             {
-                // Om användaren skapades så skapas även en rad i tabellen 'Users'
-                usersRepository.CreateUser(model, user.Id);
+                // Om användaren skapades så skapas även en rad i tabellen 'Users', 'Consultants' och 'UserJobLocation'
+                accountRepository.CompleteUser(model, user.Id);
 
                 await signInManager.PasswordSignInAsync(
                     model.Email, "P@ssw0rd", false, false);
@@ -136,12 +159,34 @@ namespace KoLappen.Controllers
             // Visa ev. fel-meddelande
             if (!result.Succeeded)
             {
-                ModelState.AddModelError(nameof(CreateUserViewModel.Email),
+                ModelState.AddModelError(nameof(RegisterUserViewModel.Email),
                     result.Errors.First().Description);
 
                 return View(model);
             }
+
             return View(model);
+        }
+
+        [AllowAnonymous]
+        public ActionResult ViewMe()
+        {
+            var model = accountRepository.GetAllConsultants();
+            return View(model);
+        }
+
+        public ActionResult CompleteRegistration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult CompleteRegistration(CompleteRegistrationViewModel model)
+        {
+            accountRepository.CompleteRegistration(model);
+            return View();
         }
     }
 }
