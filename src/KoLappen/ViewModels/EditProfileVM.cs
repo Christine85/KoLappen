05c9fd24e841733e3dfd768063e275af11166667
weibust <p.weibust@gmail.com>
2016-04-07 14:52:39 +0200

    småfix med layout....

--- src/KoLappen/ViewModels/EditProfileVM.cs
+++ src/KoLappen/ViewModels/EditProfileVM.cs
@@ -1,10 +1,16 @@
-﻿namespace KoLappen.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KoLappen.ViewModels
 {
     public class EditProfileVM
     {
+        [Display(Name ="Förnamn")]
         public string Firstname { get; set; }
+        [Display(Name ="Efternamn")]
         public string Lastname { get; set; }
+        [Display(Name ="E-mail")]
         public string Email { get; set; }
+        [Display(Name ="Telefonnummer")]
         public string Phonenumber { get; set; }
         public string UserId { get; set; }
     }
