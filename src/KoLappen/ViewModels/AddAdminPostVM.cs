87cc333c6dce84fe8a2b1d927d0b390f5a5475c4
weibust <p.weibust@gmail.com>
2016-04-07 20:53:17 +0200

    css osv

---
+++ src/KoLappen/ViewModels/AddAdminPostVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoLappen.ViewModels
+{
+    public class AddAdminPostVM
+    {
+        [Display(Name = "Text")]
+        [Required(ErrorMessage = "Skriv in text som skall publiceras.")]
+        public string PostText { get; set; }
+
+        [Display(Name = "Länk")]
+        public string Link { get; set; }
+
+        public DateTime TimePosted { get; set; }
+    }
+}
