d0567b19ff81045941b6409d053f0787eb83e48a
weibust <p.weibust@gmail.com>
2016-04-07 21:56:30 +0200

    emelie posts.......

---
+++ src/KoLappen/Controllers/AdminPostController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Authorization;
+using KoLappen.ViewModels;
+using KoLappen.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KoLappen.Controllers
+{
+    public class AdminPostController : Controller
+    {
+        IAdminPostsRepository _adminPostsRepository;
+        public AdminPostController(
+            IAdminPostsRepository adminPostsRepository)
+        {
+            _adminPostsRepository = adminPostsRepository;
+        }
+
+        // GET: /<controller>/
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //[Authorize]
+        public IActionResult AddPost()
+        {
+            return View();
+        }
+        //[Authorize]
+        [HttpPost]
+        public IActionResult AddPost(AddAdminPostVM viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            _adminPostsRepository.AddPost(viewModel, User.Identity.Name);
+            return RedirectToAction("index", "home");
+        }
+    }
+}
---
+++ src/KoLappen/Models/AdminPost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoLappen.Models
+{
+    public class AdminPost
+    {
+        public int Id { get; set; }
+        public string PostText { get; set; }
+        public string Link { get; set; }
+        public DateTime TimePosted { get; set; }
+        public string UserID { get; set; }
+        public virtual User User { get; set; }
+    }
+}
